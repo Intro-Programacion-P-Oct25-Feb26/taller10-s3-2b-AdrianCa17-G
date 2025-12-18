61ed061c149f03ab420d15e9a09df2166a8fb3d7
AdrianCa17-G <adrian.steven.guaman.carrion@gmail.com>
2025-12-18 10:49:51 -0500

    Segundo ejercicio

--- Taller10/src/paquete2/Problema02.java
+++ Taller10/src/paquete2/Problema02.java
@@ -10,5 +10,43 @@ package paquete2;
  * @author reroes
  */
 public class Problema02 {
-    
+
+    public static void main(String[] args) {
+
+        int[][] edades = {
+            {5, 12, 35, 40},
+            {8, 15, 33, 38},
+            {6, 10, 30, 36}
+        };
+
+        int[] adultos = new int[edades.length];
+
+        int niños = 0;
+
+        String reporte = "";
+
+        for (int i = 0; i < edades.length; i++) {
+
+            for (int j = 0; j < edades[i].length; j++) {
+
+                if (edades[i][j] >= 18) {
+
+                    adultos[i] = adultos[i] + 1;
+                } else {
+
+                    niños = niños + 1;
+                }
+            }
+        }
+
+        reporte = String.format("Total de menores de edad: %s\n", niños);
+
+        for (int i = 0; i < adultos.length; i++) {
+            reporte = String.format("%sAdultos en familia %d: %s\n", reporte, i+1, adultos[i]);
+
+        }
+        System.out.println(reporte);
+    }
+
 }
+
