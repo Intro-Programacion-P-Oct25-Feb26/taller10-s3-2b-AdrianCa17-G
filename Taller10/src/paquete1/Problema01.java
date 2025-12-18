cd96400d9a633eb2f8d1663559036010d310ea36
AdrianCa17-G <adrian.steven.guaman.carrion@gmail.com>
2025-12-18 10:33:33 -0500

    Primer ejercicio

--- Taller10/src/paquete1/Problema01.java
+++ Taller10/src/paquete1/Problema01.java
@@ -15,6 +15,41 @@ public class Problema01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
+        boolean[][] asistencia = {
+            {true, true, false, true, true},
+            {true, false, false, true, false},
+            {true, true, true, true, true},
+            {false, true, true, false, true}
+        };
+
+        String[] estudiantes = {"Ana", "Luis", "Carlos", "María"};
+
+        int[] dias = new int[5];
+
+        String reporte = "";
+
+        for (int i = 0; i < asistencia.length; i++) {
+
+            for (int j = 0; j < asistencia[i].length; j++) {
+                if (asistencia[i][j]) {
+                    dias[i] = dias[i] + 1;
+                }
+
+            }
+        }
+        for (int i = 0; i < estudiantes.length; i++) {
+
+            reporte = String.format("%sEstudiante: %s", reporte, estudiantes[i]);
+            reporte = String.format("%s -- Dias asistidos %s -- ", reporte, dias[i]);
+
+            if (dias[i] == 5) {
+                reporte = String.format("%sAsistio todos los dias\n", reporte);
+            } else {
+                reporte = String.format("%sNo asistio todos los dias\n", reporte);
+            }
+        }
+        System.out.println(reporte);
     }
-    
+
 }
